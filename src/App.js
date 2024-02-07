4291fbe5a85cf2ea3af004ccb9beb70df47e2eb6
Shivam kumar (Software Engineer) <96223420+Shivam265@users.noreply.github.com>
2024-02-07 03:05:49 -0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { getDatabase, push, ref, set, onChildAdded } from "firebase/database";
 
 function App() {
+  const [name, setName] = useState("");
+  const [chats, setChats] = useState([]);
+  const [msg, setMsg] = useState("");
+
+  const db = getDatabase();
+  const chatListRef = ref(db, "chats");
+
+  const updateHeight = () => {
+    const el = document.getElementById("chat");
+    if (el) {
+      el.scrollTop = el.scrollHeight;
+    }
+  };
+
+  useEffect(() => {
+    onChildAdded(chatListRef, (data) => {
+      setChats((chats) => [...chats, data.val()]);
+      setTimeout(() => {
+        updateHeight();
+      }, 1000);
+    });
+  }, []);
+
+  const sendChat = () => {
+    const chatRef = push(chatListRef);
+    set(chatRef, {
+      name,
+      message: msg,
+    });
+    // const c = [...chats];
+    // c.push({ name, message: msg });
+    // setChats(c);
+    setMsg("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {name ? null : (
+        <div>
+          <input
+            type="text"
+            placeholder="Enter in users"
+            onBlur={(e) => setName(e.target.value)}
+          ></input>
+        </div>
+      )}
+      {name ? (
+        <div>
+          <h3>User:{name}</h3>
+          <div id="chat" className="chat-container">
+            {chats.map((c, i) => (
+              <div
+                key={i}
+                className={`container ${c.name === name ? "me" : ""}`}
+              >
+                <p className="chatbox">
+                  <strong>{c.name}:</strong>
+                  <span>{c.message}</span>
+                </p>
+              </div>
+            ))}
+          </div>
+
+          <div className="btn">
+            <input
+              type="text"
+              onInput={(e) => setMsg(e.target.value)}
+              value={msg}
+              placeholder="Send a message"
+            ></input>
+
+            <button onClick={(e) => sendChat()}>Send</button>
+          </div>
+        </div>
+      ) : null}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDNQHtlHxSp3xblIQZbVfUUbFdZPdWJfyQ",
+  authDomain: "chats-app-e3c1a.firebaseapp.com",
+  databaseURL: "https://chats-app-e3c1a-default-rtdb.firebaseio.com",
+  projectId: "chats-app-e3c1a",
+  storageBucket: "chats-app-e3c1a.appspot.com",
+  messagingSenderId: "665268070931",
+  appId: "1:665268070931:web:ce54839e1caa7627b92de6",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
